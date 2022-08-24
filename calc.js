78d7e0b2aa548eafc14c4d2298da8a351f8352ee
gLitCH1904 <95301749+gLitCH1904@users.noreply.github.com>
2022-08-24 21:41:04 +0100

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,128 @@
+"use strict";
+
+// DOM elements
+const billInput = document.getElementById("inp-bill");
+const tipInput = document.getElementById("inp-tip");
+const peopleInput = document.querySelector("#inp-people");
+const btnPercentage = document.querySelectorAll(".btn");
+const errorMsg = document.querySelector(".error-msg");
+const errorMsg2 = document.querySelector(".error-msg2");
+const tipAmount = document.querySelector(".tip");
+const billTotal = document.querySelector(".bill-total");
+const reset = document.querySelector(".output-btn");
+const calc = document.querySelector(".btn-calc");
+const popUp = document.querySelector(".pop-up");
+const overlay = document.querySelector(".overlay");
+const instructions = document.querySelector(".instructions");
+const closeBtn = document.querySelector(".close-btn");
+
+// state variables
+let total = 0;
+let billValue = 0;
+let peopleValue = 1;
+let tipValue = 0;
+
+// creating event listeners that call the functions on the input elements
+btnPercentage.forEach((btn) => {
+  btn.addEventListener("click", percentageClick);
+});
+
+// billInputFun means "bill-input Function"
+billInput.addEventListener("input", billInputFun);
+peopleInput.addEventListener("input", peopleInputFun);
+tipInput.addEventListener("input", tipInputFun);
+
+// calculating the tip
+function calcTip() {
+  if (peopleValue >= 1 && billValue >= 1) {
+    let amountTipped = (billValue * tipValue) / peopleValue;
+    total = (billValue + amountTipped) / peopleValue;
+    tipAmount.textContent = `$${amountTipped.toFixed(2)}`;
+    billTotal.textContent = `$${total.toFixed(2)}`;
+  }
+}
+
+function percentageClick(event) {
+  btnPercentage.forEach((btn) => {
+    if (event.target.textContent === btn.textContent) {
+      tipValue = parseFloat(btn.textContent) / 100;
+    }
+  });
+
+  calcTip();
+}
+
+function billInputFun() {
+  billValue = parseFloat(billInput.value);
+  calcTip();
+}
+
+function tipInputFun() {
+  tipValue = parseFloat(tipInput.value) / 100;
+
+  // error message for custom-input
+  if (tipValue <= 0) {
+    addError(tipInput, errorMsg2, "be zero");
+  } else if (tipValue < 100) {
+    removeError(tipInput, errorMsg2);
+  } else if (tipValue > 100) {
+    addError(tipInput, errorMsg2, "exceed 10k");
+  }
+
+  if (tipValue < 100) {
+    calcTip();
+  }
+}
+
+function peopleInputFun() {
+  peopleValue = parseFloat(peopleInput.value);
+
+  // error message for people-input
+  if (peopleValue < 1) {
+    addError(peopleInput, errorMsg, "be zero");
+  } else if (peopleValue < 100) {
+    removeError(peopleInput, errorMsg);
+  } else if (peopleValue > 100) {
+    addError(peopleInput, errorMsg, "exceed 100");
+  }
+
+  calcTip();
+}
+
+// enabling reset button
+reset.addEventListener("click", () => {
+  tipAmount.textContent = `$0.00`;
+  billTotal.textContent = `$0.00`;
+  billInput.value = "";
+  peopleInput.value = "";
+  tipInput.value = "";
+});
+
+// function that adds the errors (error-function)
+function addError(input, msg, max) {
+  msg.textContent = `Can't ${max}`;
+  msg.classList.add("active");
+  input.classList.add("active");
+}
+
+// function that removes the errors
+function removeError(input, msg) {
+  msg.classList.remove("active");
+  input.classList.remove("active");
+}
+
+// functionality of pop-up displaying the instructions and overlay control
+popUp.addEventListener("click", () => {
+  instructions.classList.add("active");
+  overlay.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  instructions.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+  instructions.classList.remove("active");
+  overlay.classList.remove("active");
+});
